1573e2a5e66a1bbf887955b8ddf325c9e03d04cf
Ashab_Siddiqui <ashabsiddiqui952@gmail.com>
2019-12-11 21:31:53 -0800

    first commit

---
+++ superheroes.py
@@ -0,0 +1,12 @@
+
+class Ability:
+    def __init__(self, name, max_damage):
+        '''
+       Initialize the values passed into this
+       method as instance variables.
+        '''
+
+        # Assign the "name" and "max_damage"
+        # for a specific instance of the Ability class
+        self.name = name
+        self.max_damage = max_damage
